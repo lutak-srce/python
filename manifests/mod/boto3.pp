d41df4a98cbb621293012f864bc569cd4ed1d679
Jakov Sosic <jsosic@gmail.com>
2018-08-09 13:20:57 +0200

    Add python boto3 and botocore

---
+++ manifests/mod/boto3.pp
@@ -0,0 +1,4 @@
+# Class: python::mod::boto3
+class python::mod::boto3 {
+  package {'python-boto3': ensure => present, }
+}
---
+++ manifests/mod/botocore.pp
@@ -0,0 +1,4 @@
+# Class: python::mod::botocore
+class python::mod::botocore {
+  package {'python-botocore': ensure => present, }
+}
